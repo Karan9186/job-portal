14108aa5e03f7406b5d1ce828540b983ca7c75a6
karan9186 <parmarkarans7486@gmail.com>
2024-09-23 16:56:35 +0530

    done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,12 @@ import Home from "./components/home/Home";
 import Jobs from "./components/jobs/Jobs";
 import Error from "./components/Error";
 import Contact from "./components/Contact";
-<<<<<<< HEAD
-=======
+// <<<<<<< HEAD
+// =======
 
->>>>>>> 5aa9e34f455de7d65e14148a2e66512c8b26c37c
+// >>>>>>> 5aa9e34f455de7d65e14148a2e66512c8b26c37c
 import Profile from "./components/profile/Profile";
-import Footer from "./components/jobs/Footer";
+import Footer from "./components/Footer";
 import Register from "./components/register/Register";
 import Recruiter from "./components/register/Recruiter";
 import Jobseeker from "./components/register/Jobseeker";
@@ -21,7 +21,6 @@ function App() {
 
   return (
     <>
-<<<<<<< HEAD
       <store.Provider value={[login, setLogin]}>
         <Nav />
         <Routes>
@@ -37,39 +36,12 @@ function App() {
           <Route path="/login" element={<Register />} />
           <Route path="/*" element={<Error />} />
         </Routes>
-        {/* <<<<<<< HEAD */}
-        <Footer />
-        {/* ======= */}
-        <br />
-        {/* <Footer /> */}
-        {/* >>>>>>> 938e1a80ba548abe656723eca77f3729a7e24fd2 */}
-      </store.Provider>
-=======
-      <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/recruiter" element={<Recruiter />} />
-        <Route path="/jobseeker" element={<Jobseeker />} />
-        <Route path="/*" element={<Error />} />
-<<<<<<< HEAD
-    </Routes>
-      <Footer/>
 
-    
-=======
-      </Routes>
-{/* <<<<<<< HEAD */}
-      <Footer/>
-{/* ======= */}
+        <br />
+      </store.Provider>
+      <Footer />
+
       <br />
-      {/* <Footer /> */}
-{/* >>>>>>> 938e1a80ba548abe656723eca77f3729a7e24fd2 */}
->>>>>>> 5aa9e34f455de7d65e14148a2e66512c8b26c37c
->>>>>>> 6ab10b8eb27e5dc820574a453053bd766f618ebb
     </>
   );
 }
